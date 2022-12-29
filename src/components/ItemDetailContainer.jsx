081e203cb0668a294bded60b91d3738d085ab4d2
Esteban Perea <estebanperea@gmail.com>
2022-12-29 12:30:45 -0300

    Configuracion y uso de firebase para la base de datos de productos

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { customFetch } from '../utils/customFetch';
-import products from '../utils/products.json'
+// import { customFetch } from '../utils/customFetch';
+// import products from '../utils/products.json'
 import ItemDetail from './ItemDetail';
-import Loader from './Loader'
+import Loader from './Loader';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 const ItemDetailContainer = () => {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
   const {id} = useParams();
 
+  // useEffect(() => {
+  //   customFetch(products)
+  //     .then(result => { 
+  //       setLoading(false);
+  //       const product = result.find(item => item.id === parseInt(id));
+  //       setProduct(product);
+  //     })
+  // }, [id]);
+
   useEffect(() => {
-    customFetch(products)
-      .then(result => { 
+    const db = getFirestore();
+    const product = doc(db, 'products', id);
+    getDoc(product).then((snapshot) => {
+      if(snapshot.exists()){
+        setProduct({id: snapshot.id, ...snapshot.data()});
         setLoading(false);
-        const product = result.find(item => item.id === parseInt(id));
-        setProduct(product);
-      })
+      }
+    });
   }, [id]);
 
   return (
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { customFetch } from '../utils/customFetch'
-import products from '../utils/products.json'
+// import { customFetch } from '../utils/customFetch'
+// import products from '../utils/products.json'
 import ItemList from './ItemList'
 import Loader from './Loader'
+import { getDocs, collection, getFirestore, query, where } from 'firebase/firestore'
 
 const ItemListContainer = () => {
 
@@ -11,15 +12,36 @@ const ItemListContainer = () => {
   const [loading, setLoading] = useState(true);
   const {category} = useParams();
 
+  // useEffect(() => {
+  //   setLoading(true);
+  //   customFetch(products)
+  //     .then(result => { 
+  //       const products = ( category? result.filter(product => product.category === category): result);
+  //       setListProducts(products);
+  //       setLoading(false);
+  //     })
+  // }, [category]);
+
   useEffect(() => {
-    setLoading(true);
-    customFetch(products)
-      .then(result => { 
-        const products = ( category? result.filter(product => product.category === category): result);
-        setListProducts(products);
-        setLoading(false);
-      })
-  }, [category]);
+    const db = getFirestore();
+    const productsCollection = collection(db, 'products');
+    const productsFilter = category? query(productsCollection, where("category", "==", category)): productsCollection;
+    getDocs(productsFilter).then((snapshot) => {
+      setListProducts(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+    })
+    setLoading(false);
+
+  },[category]);
+
+  //proceso para cargar productos en nuestra coleccion
+  // useEffect(() => {
+  //   const db = getFirestore();
+  //   const productsCollection = collection(db, 'products');
+  //   products.forEach(product => {
+  //     addDoc(productsCollection, product);
+  //   })
+
+  // },[])
 
   return (
     <div className="container">
